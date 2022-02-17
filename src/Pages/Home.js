d7d71cde1880ee9cb4f12ff870d4c261a3601e1d
nadhirchamri <chamrinadhirr@gmail.com>
2022-02-17 21:33:33 +0100

    api

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+const Home = () => {
+  return (
+    <div>
+        <br/>
+      <h1>Welcome Home</h1>
+      <img
+            src="https://cdn.shopify.com/s/files/1/1158/2314/products/Welcome_Home_1024x1024.jpg?v=1571711903"
+  alt="home"        />
+    </div>
+  );
+};
+
+export default Home;
